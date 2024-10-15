3bbc69a14898f875cce9431c0f4500b50879a99d
linda <linda.ek@bredband2.com>
2024-10-15 09:37:31 +0200

    Added a complete liberary of skills as well as a way to call then and there atribute. All in preperation for Rolling skills and easier combat. Skills saved in a modifiable txt file. Will add ways to change that later

---
+++ attrib_lib.py
@@ -0,0 +1,14 @@
+Skills = {
+# Skills_Athletics
+"Acrobatics":5, "Athleticism":6, "Climb":7, "Jump":8, "Ride":9, "Swim":10,
+# Skills_Vigor
+"Composure":13, "Feats of str":14, "Withstand pain":15,
+# Skills_Perception
+"Notice":18, "Sertch":19, "Track":20,
+# Skills_Intellectual
+"Animals":23, "Appraisal":24, "Herbal lore":25, "History":26, "Magic appraisal":27, "Medicine":28, "Memorize":29, "Navigation":30, "Occult":31, "Sciences":32,
+# Skills_Subterfuge
+"Disguise":35, "Hide":36, "Lock picking":37, "Poisons":38, "Theft":39, "Trap lore":40, "Stealth":41,
+# Skills_Creative 
+"Art":44, "Dance":45, "Forging":46, "Music":47, "Slight of hand":48
+}
